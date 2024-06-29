5a244dcd6201ac30e61e5c09354aab4fb50703d6
ththune <135433132+ththune@users.noreply.github.com>
2024-06-29 22:22:16 +0200

    Configuration DbContext to connect with the database.

--- BookService/Data/DataContext.cs
+++ BookService/Data/DataContext.cs
@@ -4,5 +4,19 @@ namespace BookService.Data
 {
     public class DataContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+        public DataContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"),
+                    optionsBuilder => optionsBuilder.EnableRetryOnFailure());
+            }
+        }
     }
 }
