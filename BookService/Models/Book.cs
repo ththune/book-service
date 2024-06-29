9d5700067e05f10a6f546f0b893709ba791075c4
ththune <135433132+ththune@users.noreply.github.com>
2024-06-29 22:13:29 +0200

    Create Book.cs

---
+++ BookService/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace BookService.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string BookTitle { get; set; } = string.Empty;
+        public string BookIsbn { get; set; } = string.Empty;
+        public DateOnly BookPublishedDate { get; set; } = DateOnly.MaxValue;
+        public int BookCopiesAvailable { get; set; } = 0;
+
+        // Database metadata. Should this property be in the Model?
+        public DateTime BookCreatedStamp { get; set; } = DateTime.Now;
+
+        // Database metadata. Should this property be in the Model?
+        public DateTime? BookUpdatedStamp { get; set; }
+
+
+    }
+}
