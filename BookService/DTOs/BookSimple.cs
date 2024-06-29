b36b62437146dd9f19f95469116455db8d14c0b0
ththune <135433132+ththune@users.noreply.github.com>
2024-06-29 23:11:41 +0200

    Create BookSimple.cs

---
+++ BookService/DTOs/BookSimple.cs
@@ -0,0 +1,15 @@
+﻿namespace BookService.DTOs
+{
+    public class BookSimple
+    {
+        public int BookId { get; set; }
+        public string BookTitle { get; set; } = string.Empty;
+        public string BookIsbn { get; set; } = string.Empty;
+        public DateOnly BookPublishedDate { get; set; } = DateOnly.MaxValue;
+        public byte BookCopiesAvailable { get; set; } = 0;
+        public BookSimple()
+        {
+
+        }
+    }
+}
